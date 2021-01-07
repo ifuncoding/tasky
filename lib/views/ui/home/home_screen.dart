9e2990cfad73bc93f3e160c568d1348fbdc89bac
ifuncoding <hnamnguyen.hy@gmail.com>
2021-01-07 22:56:55 +0700

    home -  infinity list

--- lib/views/ui/home/home_screen.dart
+++ lib/views/ui/home/home_screen.dart
@@ -98,23 +98,85 @@ class HomeScreen extends StatelessWidget {
         body: Container(
           child: ListView.builder(
             padding: EdgeInsets.all(0),
-            itemCount: 100,
+            itemCount: 1,
             itemBuilder: (BuildContext context, int index) {
-              return Card(
-                color: index % 2 == 0 ? Colors.blue : Colors.green,
-                child: Container(
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  height: 100.0,
-                  child: Text(
-                    'Flutter is awesome',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ),
-              );
+              return TaskVerticalItem();
             },
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TaskVerticalItem extends StatelessWidget {
+  final bool isDone;
+
+  const TaskVerticalItem({
+    Key key,
+    this.isDone = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      width: double.infinity,
+      height: 75.0,
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            margin: isDone
+                ? EdgeInsets.only(right: 14)
+                : EdgeInsets.symmetric(horizontal: 18),
+            child: isDone
+                ? SvgPicture.asset('assets/icons/arrow-left-circle.svg')
+                : Stack(
+                    children: [
+                      Container(
+                        width: 3,
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: Palette.bg_item_1.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          width: 3,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            color: Palette.bg_item_1,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+          ),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomText(
+                  'Create New Features',
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+                CustomText(
+                  'Mobile Apps Design',
+                  fontSize: 15,
+                  color: Palette.text_primary.withOpacity(0.5),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          SvgPicture.asset('assets/icons/more-horizontal.svg'),
+        ],
       ),
     );
   }
